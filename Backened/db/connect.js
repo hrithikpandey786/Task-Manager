912ef453f774c6491c046e0270ab4d60178c0a77
hrithikpandey786 <hrithikpandey786@gmail.com>
2024-05-19 22:53:24 +0530

    Fixed Bug

--- Backened/db/connect.js
+++ Backened/db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-// const connectionString = 'mongodb+srv://hrithikpandey165:12hrithik@nodeexpressprojects.ughe0bd.mongodb.net/'
-
 const connectDB = (url)=>{
     return mongoose.connect(url, {
         useCreateIndex:true, 
